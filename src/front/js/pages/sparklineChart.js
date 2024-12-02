6bf20a4e367b3ef9e399e151763510ebf7de1242
Daniel Alvaro <153893558+danielta18@users.noreply.github.com>
2024-12-02 16:55:40 +0000

    favorite screen fixing

--- src/front/js/pages/sparklineChart.js
+++ src/front/js/pages/sparklineChart.js
@@ -7,7 +7,7 @@ export const SparklineChart = ({ data, width, height}) => {
     return (
         <LineChart width={width} height={height} data={chartData}>
             <YAxis type="number" domain={['dataMin', 'dataMax']} width={0} />
-            <Line type="monotone" dataKey="price" stroke="#8884d8" strokeWidth={2} dot={false} />
+            <Line type="monotone" dataKey="price" stroke="#39ff14" strokeWidth={2} dot={false} />
         </LineChart>
     );
 };
