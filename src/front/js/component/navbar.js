e3ea362d952fb76af295c14dfdf487c6bb9822b1
soledadrodriguez <68907554+soledadrodriguez@users.noreply.github.com>
2024-12-02 22:24:29 +0000

    [Primer commit]

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,17 +3,48 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+		<nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+		<div className="container">
+			<a className="navbar-brand text-primary fw-bold" href="#">
+				Monitó 🐾
+			</a>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#navbarNav"
+				aria-controls="navbarNav"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNav">
+				<ul className="navbar-nav ms-auto">
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Inicio
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Mapa
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							¿Qué es?
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Contacto
+						</a>
+					</li>
+				</ul>
+				<button className="btn btn-primary ms-3">Iniciar sesión</button>
 			</div>
-		</nav>
+		</div>
+	</nav>
 	);
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,25 +2,89 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<div>
+
+			{/* Contenido principal */}
+			<div
+				className="d-flex justify-content-center align-items-center"
+				style={{
+					height: "90vh",
+					background: "linear-gradient(to bottom right, #FCECCF, #FDF4E6)",
+					position: "relative",
+					overflow: "hidden",
+				}}
+			>
+				{/* Figuras decorativas */}
+				<div
+					style={{
+						position: "absolute",
+						width: "200px",
+						height: "200px",
+						backgroundColor: "#FDC968",
+						borderRadius: "50%",
+						top: "10%",
+						left: "20%",
+						zIndex: 1,
+					}}
+				></div>
+				<div
+					style={{
+						position: "absolute",
+						width: "300px",
+						height: "300px",
+						backgroundColor: "#002B5B",
+						borderRadius: "50%",
+						bottom: "20%",
+						right: "10%",
+						zIndex: 1,
+					}}
+				></div>
+				<div
+					style={{
+						position: "absolute",
+						width: "100px",
+						height: "100px",
+						backgroundColor: "#FDC968",
+						borderRadius: "50%",
+						bottom: "30%",
+						right: "30%",
+						zIndex: 1,
+					}}
+				></div>
+				<div
+					style={{
+						position: "absolute",
+						width: "80px",
+						height: "80px",
+						backgroundColor: "#FDC968",
+						borderRadius: "50%",
+						top: "40%",
+						left: "15%",
+						zIndex: 1,
+					}}
+				></div>
+
+				{/* Contenido textual */}
+				<div className="container text-center z-3" >
+					<h1 className=" adlam-display-regular display-5 fw-bold text-dark mb-3">
+						Una ayuda para encontrar mascotas perdidas en tu zona.
+					</h1>
+					<p className="text-muted fs-5 mb-4">
+						Encuentra a tu mascota o ayuda a otra persona a encontrarla. Miles
+						de historias felices nos respaldan.
+					</p>
+					<div className="d-flex justify-content-center gap-3">
+						<button className="btn btn-danger btn-lg">Perdí a mi mascota</button>
+						<button className="btn btn-success btn-lg">Encontré una mascota</button>
+					</div>
+				</div>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
 		</div>
 	);
 };
