b0eefbf7430eaed5cb8c38cd7d46567b4d8ab64d
Yuneido Chacin <146007178+yjchf@users.noreply.github.com>
2024-12-03 00:59:26 +0000

    Merge dev

--- src/front/js/component/leftMenuParent/Calendario.jsx
+++ src/front/js/component/leftMenuParent/Calendario.jsx
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Calendar } from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "../../../styles/Calendar.css";
-import { formatDate } from "date-fns";
-
-const HandleChange = value =>
-  alert(`New date is: ${value.toLocaleDateString()}`);
+import Overlay from "react-bootstrap/Overlay";
+import Popover from "react-bootstrap/Popover";
 
 const Calendario = ({ eventos }) => {
   const [value, setValue] = useState(new Date()); // Día seleccionado
   const [activeStartDate, setActiveStartDate] = useState(new Date()); // Mes visible
+  const [popoverInfo, setPopoverInfo] = useState({
+    show: false,
+    target: null,
+    event: null,
+  });
+  const calendarRef = useRef(null);
 
   const handleActiveStartDateChange = ({ activeStartDate }) => {
     setActiveStartDate(activeStartDate);
 
-    // Si volvemos al mes actual, seleccionamos el día actual
     const today = new Date();
     if (
       today.getFullYear() === activeStartDate.getFullYear() &&
@@ -25,39 +28,105 @@ const Calendario = ({ eventos }) => {
   };
 
   const tileClassName = ({ date, view }) => {
-    // Asegúrate de que estamos en la vista mensual
     if (view === "month") {
-      // Formateamos la fecha para comparar
       const formattedDate = date.toISOString().split("T")[0];
 
-      // Filtra los eventos que coinciden con la fecha actual
       const matchingEvents = eventos.filter(
         evento => evento.date === formattedDate
       );
 
       if (matchingEvents.length > 0) {
-        // Si hay algún evento marcado como feriado
         if (matchingEvents.some(evento => evento.holiday)) {
-          return "highlight-holiday"; // Clase CSS para feriados
+          return "highlight-holiday"; // Clase CSS para feriados y/o evaluaciones
         }
         return "highlight"; // Clase CSS para eventos regulares
       }
     }
-    return null; // Ninguna clase si no hay eventos
+    return null;
   };
 
+  const handleDayClick = (value, event) => {
+    const formattedDate = value.toISOString().split("T")[0];
+
+    // Busca los eventos para este día
+    const matchingEvents = eventos.filter(
+      evento => evento.date === formattedDate
+    );
+
+    if (matchingEvents.length > 0) {
+      // Muestra el popover con información del evento
+      setPopoverInfo({
+        show: true,
+        target: event.target, // Elemento donde se mostrará el popover
+        event: matchingEvents, // Muestra el primer evento del día (puedes ajustar esto si hay más eventos)
+      });
+    } else {
+      // Oculta el popover si no hay eventos
+      setPopoverInfo({ show: false, target: null, event: null });
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = event => {
+      if (
+        calendarRef.current &&
+        !calendarRef.current.contains(event.target) &&
+        popoverInfo.target &&
+        !popoverInfo.target.contains(event.target)
+      ) {
+        setPopoverInfo({ show: false, target: null, event: null });
+        setValue(new Date());
+      }
+    };
+
+    if (popoverInfo.show) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [popoverInfo]);
+
   return (
-    <div className="calendar-container">
+    <div className="calendar-container" ref={calendarRef}>
       <Calendar
         onActiveStartDateChange={handleActiveStartDateChange}
-        onClickDay={(value, e) => {
-          console.log(`Day pressed: ${value}`);
-          console.log(formatDate(value, "yyyy-mm-dd"));
-        }}
+        onClickDay={handleDayClick}
         onChange={setValue}
         tileClassName={tileClassName}
         value={value}
       />
+
+      <Overlay
+        show={popoverInfo.show}
+        target={popoverInfo.target}
+        placement="right"
+        containerPadding={20}>
+        <Popover id="popover-basic">
+          <Popover.Header as="h3">
+            {popoverInfo.event?.map(e => e.title + " ") ||
+              "Información del Evento"}
+          </Popover.Header>
+          <Popover.Body>
+            {popoverInfo.event?.map(e => {
+              if (e.holiday) {
+                return (
+                  <div>
+                    {e.title} <br /> {e.date}
+                  </div>
+                );
+              }
+
+              return (
+                <p>
+                  {e.materia} - Grado: {e.grado} <br /> {e.profesor}
+                </p>
+              );
+            })}
+          </Popover.Body>
+        </Popover>
+      </Overlay>
     </div>
   );
 };
--- src/front/js/pages/ParentDashboard.jsx
+++ src/front/js/pages/ParentDashboard.jsx
@@ -4,8 +4,10 @@ import styled from "styled-components";
 import img from "./../../img/background.jpg";
 import { Context } from "../store/appContext.js";
 import MainDashboard from "../component/leftMenuParent/MainDashboard.jsx";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import ProfileForm from "../component/ProfileForm.jsx";
 
-let Content = styled.div`
+const Content = styled.div`
   flex: 1;
   padding: 1rem;
   background-image: url(${img});
@@ -17,12 +19,12 @@ let Content = styled.div`
 const ParentDashboard = () => {
   const [activeKey, setActiveKey] = useState("home");
   const { store, actions } = useContext(Context);
-  const [info, setInfo] = useState("");
   const [infoEventos, setInfoEventos] = useState([]);
   const [infoEstudiantes, setInfoEstudiantes] = useState([]);
+  const navigate = useNavigate();
+
   const handleSelect = key => {
-    setActiveKey(key);
-    console.log("Selected:", key);
+    navigate(key);
   };
 
   useEffect(() => {
@@ -58,14 +60,35 @@ const ParentDashboard = () => {
   };
 
   return (
-    <div style={{ display: "flex", height: "100vh", paddingTop: "100px" }}>
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        paddingTop: "100px",
+      }}>
       <ParentSideBar
         items={menuItems}
         activeKey={activeKey}
         onSelect={handleSelect}
       />
 
-      <Content>{handleContentRender(activeKey)}</Content>
+      <Content>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <MainDashboard
+                dataEvents={infoEventos}
+                estudiantes={infoEstudiantes}
+              />
+            }
+          />
+          <Route
+            path="/profile"
+            element={<ProfileForm user={store.personalInfo ?? {}} />}
+          />
+        </Routes>
+      </Content>
     </div>
   );
 };
@@ -74,23 +97,28 @@ export default ParentDashboard;
 
 const menuItems = [
   {
-    key: "main",
+    key: "/dashboard/parent",
     label: "Dashboard",
     icon: <i className="bi bi-speedometer2"></i>,
   },
   {
-    key: "review",
+    key: "/dashboard/parent/",
     label: "Revisar",
     icon: <i className="bi bi-journal-check"></i>,
   },
   {
-    key: "settings",
+    key: "/dashboard/parent/",
     label: "Settings",
     icon: <i className="bi bi-journal-text"></i>,
   },
   {
-    key: "messages",
+    key: "/dashboard/parent/",
     label: "Mensajes",
+    icon: <i className="bi bi-chat-left-text"></i>,
+  },
+  {
+    key: "/dashboard/parent/profile",
+    label: "Perfil",
     icon: <i className="bi bi-chat-left-text"></i>,
   },
 ];
