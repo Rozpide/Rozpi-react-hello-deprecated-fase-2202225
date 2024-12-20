4370ea9007348599b34efe052bd8b84b5c16baba
Santiago Montoya <santmordz@MacBook-Air-de-Santiago.local>
2024-12-20 17:14:34 -0500

    styles: password recovery background design

--- src/front/js/component/PasswordRecovery.jsx
+++ src/front/js/component/PasswordRecovery.jsx
@@ -2,10 +2,14 @@ import React, { useContext, useState } from "react";
 import styled from "styled-components";
 import Swal from "sweetalert2";
 import { Context } from "../store/appContext";
+import logginBackground from "../../img/logginBack.png"
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  padding-right: 1.5rem;
+  padding-left: 1.5rem;
+  padding-top: 1.5rem;
   height: 60vh;
   aspect-ratio: 1 / 1;
   background-image: linear-gradient(
@@ -22,7 +26,7 @@ export const Background = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background: linear-gradient(180deg, #0a3d62, #3c6382);
+  background-image: linear-gradient(to right, rgba(31, 118, 146, 0.5), rgba(67, 56, 135, 0.5));
   height: 100vh;
 `;
 
@@ -45,58 +49,60 @@ const PasswordRecovery = () => {
       if (response) {
         Swal.fire({
           title:
-            "Se ha enviado un enlace al correo para recuperacion de contraseña",
+            "Se ha enviado un enlace al correo para recuperación de contraseña",
           icon: "success",
           text: "Ingresa a tu correo para continuar con el proceso",
         });
       }
     } catch (error) {
       Swal.fire({
-        title: "Error al solicitar recuperacion de contraseña",
+        title: "Error al solicitar la recuperación de contraseña",
         icon: "error",
-        text: error.message || "Error al realizar l solicitud",
+        text: error.message || "Error al realizar la solicitud",
         timer: 1500,
       });
     }
   };
 
   return (
-    <Background>
-      <Container>
-        <form onSubmit={e => handleSubmit(e)}>
-          <div className="container-fluid">
-            <div className="row mb-3">
-              <h4 className="m-3 text-center">¿Olvidaste tu contraseña?</h4>
-              <p className="text-light fs-6 fw-light text-center">
-                Ingresa tu correo de inicio de sesion para recuperar la
-                contraseña
-              </p>
-            </div>
-            <div className="row mt-2">
-              <div className="col-12">
-                <label htmlFor="email" className="form-label">
-                  Correo Electronico
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  className="form-control rounded-pill"
-                  placeholder="micorreo@dominio.com"
-                  value={emailInput}
-                  onChange={e => setEmailInput(e.currentTarget.value)}
-                  required
-                />
+    <div style={{ backgroundImage: `url(${logginBackground})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
+      <Background >
+        <Container>
+          <form onSubmit={e => handleSubmit(e)}>
+            <div className="container-fluid">
+              <div className="row mb-3">
+                <h4 className="m-3 text-center">¿Olvidaste tu contraseña?</h4>
+                <p className="text-light fs-6 fw-light text-center">
+                  Ingresa tu correo de inicio de sesión para recuperar la
+                  contraseña.
+                </p>
+              </div>
+              <div className="row mt-2">
+                <div className="col-12">
+                  <label htmlFor="email" className="form-label">
+                    Correo electrónico:
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="form-control rounded-pill mb-3"
+                    placeholder="micorreo@dominio.com"
+                    value={emailInput}
+                    onChange={e => setEmailInput(e.currentTarget.value)}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="row mt-3">
+                <div className="col-12 text-center">
+                  <button className="btn btn-outline-register">Recuperar</button>
+                </div>
               </div>
             </div>
-            <div className="row mt-3">
-              <div className="col-12 text-center">
-                <button className="btn btn-outline-register">Recuperar</button>
-              </div>
-            </div>
-          </div>
-        </form>
-      </Container>
-    </Background>
+          </form>
+        </Container>
+      </Background>
+    </div>
   );
 };
 
