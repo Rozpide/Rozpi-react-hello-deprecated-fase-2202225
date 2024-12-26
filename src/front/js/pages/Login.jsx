dc210683a675759ac7ea9399be4f96883b1d5412
Joni Santos <jonisantos1996@hotmail.com>
2024-12-26 18:22:58 +0000

    [revision] initial changes

---
+++ src/front/js/pages/Login.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import JourNaviLogo from "../../img/JourNavi Logo.png";
+
+
+export const Login = () => {
+    const { store } = useContext(Context);
+
+    return (
+        <main className="form-signin w-100 m-auto">
+        <form>
+            <img className="mb-4" src={JourNaviLogo} alt="" width="72" height="57" />
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+            <div className="form-floating">
+                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                <label for="floatingPassword">Password</label>
+            </div>
+
+            <div className="form-check text-start my-3">
+                <input className="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault" />
+                <label className="form-check-label" for="flexCheckDefault">
+                    Remember me
+                </label>
+            </div>
+            <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+            <p className="mt-5 mb-3 text-body-secondary">© 2017–2024</p>
+        </form>
+        </main>
+    )
+}
