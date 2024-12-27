42a75801d6d437e4f75dc8146e351b9614cf0e57
Joni Santos <jonisantos1996@hotmail.com>
2024-12-27 12:05:03 +0000

    [feat] login structure

--- src/front/js/component/Footer.jsx
+++ src/front/js/component/Footer.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer mt-auto py-3 text-center" style={{background: '#FE5558'}}>
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			Made with <i className="fa fa-heart text-danger" /> by <a style={{ textDecoration: 'none' }} href="https://github.com/JoniXSantos">Joni Santos</a> & <a style={{ textDecoration: 'none' }} href="http://www.4geeksacademy.com">4Geeks Academy</a>
 		</p>
 	</footer>
 );
--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-light"  style={{background: '#FE5558'}}>
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<Link to="/" style={{ textDecoration: 'none' }}>
+					<span className="navbar-brand mb-0 h1">JourNavi</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+					<Link to="/login">
+						<button className="btn btn-dark">Login</button>
 					</Link>
 				</div>
 			</div>
