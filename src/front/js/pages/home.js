15fa283b0886ceef5cf897bea75f2b6cdfa7bf38
José Navarro <joseng1997@gmail.com>
2025-02-03 22:59:52 +0000

    chore: avance en css home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,9 +8,16 @@ export const Home = () => {
 
 	return (
 		<div className="home-container">
-			<section className="section home__section-1 " style={{ backgroundImage: `url(${process.env.BACKEND_URL + "/padel-court.jpg"})` }}>
-				<h1>Sección 1</h1>
-				<p>Contenido de la primera sección.</p>
+			<section
+				className="section home__section-1 position-relative vh-50 d-flex align-items-center justify-content-center"
+				style={{backgroundImage: `url(${process.env.BACKEND_URL + "/padel-court.jpg"})`}}
+			>
+				<div className="position-absolute bottom-0 start-50 translate-middle-x text-center w-100 mb-5 ">
+					<h1>¿Qué es PadelZone?</h1>
+					<p className="mt-3">
+						La plataforma perfecta para organizar y participar en eventos deportivos, diseñada para la creación y organización de torneos
+					</p>
+				</div>
 			</section>
 			<section className="section section-2">
 				<h1>Sección 2</h1>
