2a97f572b7d6615e0295dcfd811cab6f70893515
José Navarro <joseng1997@gmail.com>
2025-02-03 12:05:07 +0000

    feat: crear secciones en home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,8 +7,19 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello world!</h1>
+		<div className="home-container">
+			<section className="section home__section-1 " style={{ backgroundImage: `url(${process.env.BACKEND_URL + "/padel-court.jpg"})` }}>
+				<h1>Sección 1</h1>
+				<p>Contenido de la primera sección.</p>
+			</section>
+			<section className="section section-2">
+				<h1>Sección 2</h1>
+				<p>Contenido de la segunda sección.</p>
+			</section>
+			<section className="section section-3">
+				<h1>Sección 3</h1>
+				<p>Contenido de la tercera sección.</p>
+			</section>
 		</div>
 	);
 };
