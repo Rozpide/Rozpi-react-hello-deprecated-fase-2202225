d81e878f9b8f24735ca3107a3406545cb8de9130
Dorian Zuluaga <166706826+Dorianzuluaga@users.noreply.github.com>
2025-02-04 22:53:51 +0000

    cambios hechos en Css, aboutUs terminado

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,31 +5,31 @@ import logo from "../../img/logo.jpg"
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light">
+		<nav className="navbar">
 			<div className="container">
 				<Link className="logoHome" to="/home">
-							  <img className="image"
-								src={logo}
-							  />
+					<img className="image"
+						src={logo}
+					/>
 				</Link>
 				<Link to="">
-				    <button className="btn ">Home</button>
-					</Link>
-					<Link to="">
-				    <button className="btn ">Cuídate</button>
-					</Link>
-					<Link to="">
-				    <button className="btn ">Tienda</button>
-					</Link>
-					<Link to="">
-				    <button className="btn ">Blog</button>
-					</Link>
-					<Link to="/aboutus">
-						<button className="btn">Conócenos</button>
-					</Link>
+					<button className="btn ">Home</button>
+				</Link>
+				<Link to="">
+					<button className="btn ">Cuídate</button>
+				</Link>
+				<Link to="">
+					<button className="btn ">Tienda</button>
+				</Link>
+				<Link to="">
+					<button className="btn ">Blog</button>
+				</Link>
+				<Link to="/aboutus">
+					<button className="btn">Conócenos</button>
+				</Link>
 				<div className="registerArea ml-auto">
 					<Link to="/login">
-				    <button className="btn btn-primary">LogIn</button>
+						<button className="btn btn-primary">LogIn</button>
 					</Link>
 				</div>
 			</div>
