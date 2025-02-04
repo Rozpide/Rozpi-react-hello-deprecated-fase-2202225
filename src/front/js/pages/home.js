d39cf4a3ec74a3090855b95ff0e1e696d1e5d29e
José Navarro <joseng1997@gmail.com>
2025-02-04 11:20:46 +0000

    chore:css home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -9,23 +9,84 @@ export const Home = () => {
 	return (
 		<div className="home-container">
 			<section
-				className="section home__section-1 position-relative vh-50 d-flex align-items-center justify-content-center"
-				style={{backgroundImage: `url(${process.env.BACKEND_URL + "/padel-court.jpg"})`}}
+				className="section home__section-1 position-relative d-flex align-items-center justify-content-center"
+				style={{ backgroundImage: `url(${process.env.BACKEND_URL + "/padel-court.jpg"})` }}
 			>
-				<div className="position-absolute bottom-0 start-50 translate-middle-x text-center w-100 mb-5 ">
+				<div className="position-absolute bottom-0 start-50 translate-middle-x text-center w-100 mb-5 home__section-1--bg">
 					<h1>¿Qué es PadelZone?</h1>
 					<p className="mt-3">
 						La plataforma perfecta para organizar y participar en eventos deportivos, diseñada para la creación y organización de torneos
 					</p>
 				</div>
 			</section>
-			<section className="section section-2">
-				<h1>Sección 2</h1>
-				<p>Contenido de la segunda sección.</p>
+			<section className="section home__section-2">
+				<h1>Últimos torneos añadidos</h1>
+				<div className="container">
+					<div className="row mt-5">
+						<div className="col-md-4">
+							<div className="card h-100 d-flex flex-column">
+								<div className="card-body d-flex flex-column p-4">
+									<h1 className="card-title">
+									{store.tournaments.length > 0 ? store.tournaments[store.tournaments.length - 1].name : "Torneo 1"}
+									</h1>
+									<p className="card-text flex-grow-1">Dirección de la ubicación 1</p>
+									<a href="#" className="btn btn-primary align-self-center mt-auto">Información</a>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-4">
+							<div className="card h-100 d-flex flex-column">
+								<div className="card-body d-flex flex-column p-4">
+									<h1 className="card-title">Ubicación 2</h1>
+									<p className="card-text flex-grow-1"></p>
+									<a href="#" className="btn btn-primary align-self-center mt-auto">Información</a>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-4">
+							<div className="card h-100 d-flex flex-column">
+								<div className="card-body d-flex flex-column p-4">
+									<h1 className="card-title">Ubicación 3</h1>
+									<p className="card-text flex-grow-1">Dirección de la ubicación 3</p>
+									<a href="#" className="btn btn-primary align-self-center mt-auto">Información</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</section>
-			<section className="section section-3">
-				<h1>Sección 3</h1>
-				<p>Contenido de la tercera sección.</p>
+			<section className="section section-3 bg-light vh-25">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-4">
+							<div className="card h-100 d-flex flex-column bg-dark text-white">
+								<div className="card-body d-flex flex-column p-4">
+									<h5 className="card-title text-center">Encuentra Torneos</h5>
+									<p className="card-text flex-grow-1">Encuentra torneos en tu ciudad y participa de forma fácil y sencilla</p>
+									<a href="#" className="btn btn-warning align-self-center mt-auto">Explorar</a>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-4">
+							<div className="card h-100 d-flex flex-column bg-dark text-white">
+								<div className="card-body d-flex flex-column">
+									<h5 className="card-title text-center">Crea Eventos</h5>
+									<p className="card-text flex-grow-1">Crea eventos deportivos de forma sencilla gracias a nuestras herramientas de creación de eventos personalizados</p>
+									<a href="#" className="btn btn-warning align-self-center mt-auto">Explorar</a>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-4">
+							<div className="card h-100 d-flex flex-column bg-dark text-white">
+								<div className="card-body d-flex flex-column">
+									<h5 className="card-title text-center">Conecta con jugadores</h5>
+									<p className="card-text flex-grow-1">Participa solo, con amigos, o tal vez prefieras encontrar nuevos compañeros de tu nivel dentro de nuestra plataforma</p>
+									<a href="#" className="btn btn-warning align-self-center mt-auto">Explorar</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</section>
 		</div>
 	);
