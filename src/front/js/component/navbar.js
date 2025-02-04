d898efbbcccf2b9e81613a10213245f7576be398
Davidgmzc <davidgomz.c13@gmail.com>
2025-02-04 16:46:10 +0000

    He echo algunos cambios.

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css"
+import logo from "../../img/logo.jpg"
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-light">
 			<div className="container">
-				<Link to="/home">
-					<span className="navbar-brand mb-0 h1">Txamanguillos</span>
+				<Link className="logoHome" to="/home">
+							  <img className="image"
+								src={logo}
+							  />
 				</Link>
-				<div className="ml-auto">
-					<Link to="/login">
-				    <button className="btn btn-primary">LogIn</button>
+				<Link to="">
+				    <button className="btn ">Home</button>
+					</Link>
+					<Link to="">
+				    <button className="btn ">Cuídate</button>
+					</Link>
+					<Link to="">
+				    <button className="btn ">Tienda</button>
+					</Link>
+					<Link to="">
+				    <button className="btn ">Blog</button>
 					</Link>
 					<Link to="/aboutus">
-						<button className="btn btn-primary m-1">Conócenos</button>
+						<button className="btn">Conócenos</button>
+					</Link>
+				<div className="registerArea ml-auto">
+					<Link to="/login">
+				    <button className="btn btn-primary">LogIn</button>
 					</Link>
 				</div>
 			</div>
