fcbc7eae83b0e5263be81048dfb8086750967249
ViesK <109549664+ViesK@users.noreply.github.com>
2025-02-07 06:08:43 +0000

    css

--- src/front/js/component/BracketCard8.jsx
+++ src/front/js/component/BracketCard8.jsx
@@ -11,14 +11,14 @@ export const BracketCard8 = () => {
     const semiFinal2 = matches[1]?.participants_match[0];
 
     return (
-        <div className="tournament-bracket container p-4">
+        <div className="bracketCard__container p-4">
             <div className="row justify-content-between d-flex mx-0">
 
                 {/* ///////////////////////////// SEMI FINALES ///////////////////////////// */}
-                <div className="col-md-3 round d-flex flex-column mb-4">
+                <div className="col-md-3 bracketCard__round d-flex flex-column mb-4">
 
                     <div>
-                        <h3 className="text-center mb-4 round-title">Semi Finales</h3>
+                        <h3 className=" bracketCard__h3 text-center mb-4 bracketCard__bracketCard__roundTitle">Semi Finales</h3>
                         <hr className="text-primary" />
                     </div>
 
@@ -29,12 +29,12 @@ export const BracketCard8 = () => {
                             <div>
                                 <div className="matchup card mb-2">
                                     <div className="d-flex align-items-center">
-                                        <p className="team m-2 m-0">Equipo {semiFinal1?.team_1?.team_number || ""}</p>
+                                        <p className="bracketCard__team m-2 m-0">Equipo {semiFinal1?.bracketCard__team_1?.bracketCard__team_number || ""}</p>
                                     </div>
                                 </div>
                                 <div className="matchup card">
                                     <div className="d-flex align-items-center">
-                                        <p className="team m-2 m-0">Equipo {semiFinal1?.team_2?.team_number || ""}</p>
+                                        <p className="bracketCard__team m-2 m-0">Equipo {semiFinal1?.bracketCard__team_2?.bracketCard__team_number || ""}</p>
                                     </div>
                                 </div>
                             </div>
@@ -45,12 +45,12 @@ export const BracketCard8 = () => {
                             <div>
                                 <div className="matchup card mb-2">
                                     <div className="d-flex align-items-center">
-                                        <p className="team m-2 m-0">Equipo {semiFinal2?.team_1?.team_number || ""}</p>
+                                        <p className="bracketCard__team m-2 m-0">Equipo {semiFinal2?.bracketCard__team_1?.bracketCard__team_number || ""}</p>
                                     </div>
                                 </div>
                                 <div className="matchup card">
                                     <div className="d-flex align-items-center">
-                                        <p className="team m-2 m-0">Equipo {semiFinal2?.team_2?.team_number || ""}</p>
+                                        <p className="bracketCard__team m-2 m-0">Equipo {semiFinal2?.bracketCard__team_2?.bracketCard__team_number || ""}</p>
                                     </div>
                                 </div>
                             </div>
@@ -60,10 +60,10 @@ export const BracketCard8 = () => {
 
 
                 {/* ///////////////////////////// FINAL ///////////////////////////// */}
-                <div className="col-md-3 round d-flex flex-column mb-4">
+                <div className="col-md-3 bracketCard__round d-flex flex-column mb-4">
 
                     <div>
-                        <h3 className="text-center mb-4 round-title">Final</h3>
+                        <h3 className=" bracketCard__h3 text-center mb-4 bracketCard__bracketCard__roundTitle">Final</h3>
                         <hr className="text-primary" />
                     </div>
 
@@ -71,13 +71,13 @@ export const BracketCard8 = () => {
 
                         <div className="w-100 mb-3 pb-md-4">
                             <div className="matchup card">
-                                <p className="team m-2 m-0">Ganador SF 1</p>
+                                <p className="bracketCard__team m-2 m-0">Ganador SF 1</p>
                             </div>
                         </div>
 
                         <div className="w-100 mt-md-3 pt-md-4 mt-4">
                             <div className="matchup card">
-                                <p className="team m-2 m-0">Ganador SF 2</p>
+                                <p className="bracketCard__team m-2 m-0">Ganador SF 2</p>
                             </div>
                         </div>
 
@@ -87,16 +87,16 @@ export const BracketCard8 = () => {
 
 
                 {/* ///////////////////////////// GANADOR ///////////////////////////// */}
-                <div className="col-md-3 round d-flex flex-column mb-4">
+                <div className="col-md-3 bracketCard__round d-flex flex-column mb-4">
 
                     <div >
-                        <h3 className="text-center mb-4 round-title">Ganador</h3>
+                        <h3 className="bracketCard__h3 text-center mb-4 bracketCard__bracketCard__roundTitle">Ganador</h3>
                         <hr className="text-primary" />
                     </div>
 
                     <div className="d-flex flex-grow-1 align-items-center w-100">
                         <div className="matchup card mb-2 w-100">
-                            <p className="team m-2 m-0">Ganador</p>
+                            <p className="bracketCard__team m-2 m-0">Ganador</p>
                         </div>
                     </div>
 
