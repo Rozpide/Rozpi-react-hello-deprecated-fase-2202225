b837d957dab2a13e655876351ef955d3190eddfb
Cecilia Terra <catreanden@gmail.com>
2025-02-10 21:58:21 +0000

    vistas iniciadas

---
+++ src/front/js/component/GameList.js
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/styles/gameList.css";
+
+
+export const GameList = () => {
+    const { store } = useContext(Context);
+
+    return (
+        <div className="game-list-container">
+            <div className="filters">
+                <button>Rating</button>
+                <button>Relevancia</button>
+                <button>Precio</button>
+                <button>Sorpréndeme</button>
+                <button className="advanced-search">Búsqueda avanzada</button>
+            </div>
+            <div className="games-table">
+                {store.videogames?.map((game, index) => (
+                    <div key={index} className="game-row">
+                        <img src={game.image} alt={game.name} className="game-image" />
+                        <div className="game-info">
+                            <h4>{game.name}</h4>
+                            <p>Información extra</p>
+                        </div>
+                        <button className="favorite-btn">❤️</button>
+                        <button className="price-btn">Precio</button>
+                    </div>
+                ))||[]}
+            </div>
+        </div>
+    );
+};
+
+
+
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/styles/navbar.css"
+
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="navbar">
+            <div className="container">
+                <Link to="/" className="logo">All <span>Games DB</span></Link>
+                <input type="text" className="search-bar" placeholder="Search" />
+                <div className="nav-buttons">
+                    <button className="favorites">⭐ Favoritos</button>
+                    <button className="logout">🔴 Logout</button>
+                </div>
+            </div>
+        </nav>
+    );
 };
+
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// export const Navbar = () => {
+// 	return (
+// 		<nav className="navbar navbar-light bg-light">
+// 			<div className="container">
+// 				<Link to="/">
+// 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+// 				</Link>
+// 				<div className="ml-auto">
+// 					<Link to="/demo">
+// 						<button className="btn btn-primary">Check the Context in action</button>
+// 					</Link>
+// 				</div>
+// 			</div>
+// 		</nav>
+// 	);
+// };
