e4d570706bf6579d3dedf1db56a3e0f8d17c58ed
josemartinbm99 <josemartinbm99@gmail.com>
2025-02-10 09:53:41 +0000

    alertas hechas

--- src/front/js/pages/signup.jsx
+++ src/front/js/pages/signup.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "../../styles/signup.css"
+import "../../styles/signup.css";
 import { Button } from "react-bootstrap";
 import { Loader } from "../component/loader";
 import Swal from "sweetalert2";
-
 
 export const SignUp = () => {
   const [form, setForm] = useState({ username: "", email: "", password: "" });
@@ -12,35 +11,26 @@ export const SignUp = () => {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
 
-
   const actualizador = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = async (e) => {
-    e.preventDefault(); 
-
-    
-    
-
+    e.preventDefault();
+    setIsLoading(true);
     try {
       const response = await fetch(`${process.env.BACKEND_URL}/api/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
-
-      }); 
-      const data = await response.json(); 
-
       });
       
-    
-
+      const data = await response.json();
 
       if (!response.ok) {
         throw new Error(data.message || "Error al registrar usuario");
       }
-    
+
       Swal.fire({
         title: "Registro exitoso",
         text: "¡Ahora puedes iniciar sesión!",
@@ -53,11 +43,8 @@ export const SignUp = () => {
         },
       });
 
-      
-  
       setError(null);
       navigate("/login");
-  
     } catch (err) {
       Swal.fire({
         title: "Error",
@@ -70,33 +57,25 @@ export const SignUp = () => {
           confirmButton: "error-button",
         },
       });
+    } finally {
+      setIsLoading(false);
     }
   };
-  
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, 1500);
 
-    return () => clearTimeout(timer); 
+    return () => clearTimeout(timer);
   }, []);
 
-const inicioLoader = async () => {
-  setIsLoading(true);
-    await waitingWearever();
-    setIsLoading(false);
-
-}
-
-useEffect(() => {
-    inicioLoader();
-}, []) 
-
-
-  return  (isLoading) ? <Loader /> : (
+  return isLoading ? (
+    <Loader />
+  ) : (
     <section className="container">
       <h1>Registro</h1>
-      <form onSubmit={handleSubmit}> {}
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="username"
@@ -122,7 +101,9 @@ useEffect(() => {
           required
         />
         {error && <p className="error">{error}</p>}
-        <Button className="button1" type="submit">Registrarse</Button>
+        <Button className="button1" type="submit">
+          Registrarse
+        </Button>
       </form>
     </section>
   );
