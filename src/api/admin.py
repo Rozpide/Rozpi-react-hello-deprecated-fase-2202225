657360199cd235dcdc2b50187841cc152814a784
unduashe <unduashe@outlook.es>
2025-02-11 15:44:08 +0000

    tablas de juegos, favoritos, tags y usuarios creadas y relacionadas

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Favourites, Tags, Games
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,10 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Favourites, db.session))
+    admin.add_view(ModelView(Tags, db.session))
+    admin.add_view(ModelView(Games, db.session))
+
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,26 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey, Table, String, DateTime, Integer, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
+from datetime import datetime
 
 db = SQLAlchemy()
 
+tags_games_association_table = db.Table(
+    "tags_games_association_table",
+    db.Model.metadata,
+    db.Column("games_id", ForeignKey("games.id"), primary_key=True),
+    db.Column("tags_id", ForeignKey("tags.id"), primary_key=True),
+)
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    __tablename__ = "user"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(80), nullable=False)
+    # relación con favourites
+    favourites: Mapped[List["Favourites"]] = relationship()
+    # last_login: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +29,78 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "favourites": [favourite.serialize() for favourite in self.favourites] if self.favourites else None
             # do not serialize the password, its a security breach
+        }
+
+class Favourites(db.Model):
+    __tablename__ = "favourites"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), nullable=False)
+    # relación con user
+    user_favourites_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    # relación con Games
+    favourite_game_id: Mapped[int] = mapped_column(ForeignKey("games.id"))
+    favourite_game: Mapped["Games"] = relationship(back_populates="favourited")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "favourite_game": self.favourite_game.serialize() if self.favourite_game else None,
+        }
+
+class Tags(db.Model):
+    __tablename__ = "tags"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tag_name: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+    # relación con games
+    games: Mapped[List["Games"]] = relationship(secondary=tags_games_association_table, back_populates="game_tags")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "tag_name": self.tag_name,
+            "games": [game.games_serialize() for game in self.games] if self.games else None
+        }
+
+    def game_serialize(self):
+        return {
+            "tag_name": self.tag_name,
+        }
+
+class Games(db.Model):
+    __tablename__ = "games"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    app_id: Mapped[int] = mapped_column(Integer, nullable=True)
+    release: Mapped[str] = mapped_column(String(100), nullable=True)
+    image_id: Mapped[str] = mapped_column(String(255), nullable=True)
+    score: Mapped[float] = mapped_column(Float, nullable=True)
+    g2a_price: Mapped[float] = mapped_column(Float, nullable=True)
+    g2a_url: Mapped[str] = mapped_column(String(250), nullable=True)
+    steam_price: Mapped[float] = mapped_column(Float, nullable=True)
+    # tags: Mapped[List[String]] = mapped_column(nullable=True)
+    # relación con favoritos
+    favourited: Mapped[List["Favourites"]] = relationship(back_populates="favourite_game")
+    # relación con tags
+    game_tags: Mapped[List["Tags"]] = relationship(secondary=tags_games_association_table, back_populates="games")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "app_id": self.app_id,
+            "release": self.release,
+            "image_id": self.image_id,
+            "score": self.score,
+            "g2a_price": self.g2a_price,
+            "g2a_url": self.g2a_url,
+            "steam_price": self.steam_price,
+            "game_tags": [tag.game_serialize() for tag in self.game_tags] if self.game_tags else None
+        }
+
+    def games_serialize(self):
+        return{
+            "name": self.name
         }
