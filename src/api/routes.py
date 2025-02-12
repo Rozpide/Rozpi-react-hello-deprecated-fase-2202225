943c68ceaba56efeb8acf7dea7562d8c2d163a71
unduashe <unduashe@outlook.es>
2025-02-12 21:22:08 +0000

    Endpoint Post funcional

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Games, Tags, Favourites
 from api.utils import generate_sitemap, APIException
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 from flask_cors import CORS
 
 api = Blueprint('api', __name__)
@@ -38,22 +39,32 @@ def post_game():
     try:
         game = db.session.execute(db.select(Games).filter_by(name=name)).scalar_one()
         if game:
-            return jsonify({'msg': "game already exists, did you mean to do a PUT?"}), 400
-    except:
+            return jsonify({"msg": "game already exists, did you mean to do a PUT?"}), 400
+    except NoResultFound:
         pass
+    except MultipleResultsFound:
+        return jsonify({"msg": "game already exists, did you mean to do a PUT?"}), 400
+    except Exception as e:
+        print("Error al intentar postear juego: ", e)
+        return jsonify({"msg": "Algo no ha salido como debería"}), 400
     tags = []
     if not isinstance(tags_id, list):
-            return jsonify({"error": "residents_id must be a list"}), 400
-
+            return jsonify({"error": "tags must be a list"}), 400
+    for tag_id in tags_id:
+        try:
+            db.session.execute(db.select(Tags).filter_by(id=tag_id)).scalar_one()
+        except NoResultFound:
+            print(f"tag_id not found in database {tag_id}")
+        except:
+            pass  
     try:
         tags = db.session.scalars(db.select(Tags).filter(Tags.id.in_(tags_id))).all()
-    except:
+    except Exception as e:
+        print("Error al intentar postear juego: ", e)
         pass
-    # data = db.session.scalars(db.select(Games)).all()
-    # results = list(map(lambda item: item.serialize(), data))
     result = Games(
         name = request_data.get('name'),
-        app_id = request_data.get('app_id'),
+        app_id = request_data.get('appId'),
         release = request_data.get('release'),
         image_id = request_data.get('imageID'),
         score = request_data.get('score'),
@@ -64,7 +75,7 @@ def post_game():
     )
     db.session.add(result)
     db.session.commit()
-    return jsonify(result.serialize()), 200
+    return jsonify(result.serialize()), 201
 
 
 @api.route("/tags", methods=['GET'])
