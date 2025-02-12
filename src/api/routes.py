073aef9db008643a21d25f05f3797a942aeecbf6
unduashe <unduashe@outlook.es>
2025-02-12 19:38:15 +0000

    se añaden endpoints de prueba post de games y get minimos

--- src/api/routes.py
+++ src/api/routes.py
@@ -14,9 +14,63 @@ CORS(api)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
-
+    data = db.session.scalars(db.select(User)).all()
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
 
+    return jsonify(response_body), 200
+
+@api.route("/games", methods=['GET'])
+def get_all_games():
+    data = db.session.scalars(db.select(Games)).all()
+    results = list(map(lambda item: item.serialize(), data))
+    response_body = {
+        "results": results
+    }
+    return jsonify(response_body), 200
+
+@api.route("/games", methods=['POST'])
+def post_game():
+    request_data = request.json
+    name = request_data.get('name')
+    tags_id = request_data.get('tags')
+    try:
+        game = db.session.execute(db.select(Games).filter_by(name=name)).scalar_one()
+        if game:
+            return jsonify({'msg': "game already exists, did you mean to do a PUT?"}), 400
+    except:
+        pass
+    tags = []
+    if not isinstance(tags_id, list):
+            return jsonify({"error": "residents_id must be a list"}), 400
+
+    try:
+        tags = db.session.scalars(db.select(Tags).filter(Tags.id.in_(tags_id))).all()
+    except:
+        pass
+    # data = db.session.scalars(db.select(Games)).all()
+    # results = list(map(lambda item: item.serialize(), data))
+    result = Games(
+        name = request_data.get('name'),
+        app_id = request_data.get('app_id'),
+        release = request_data.get('release'),
+        image_id = request_data.get('imageID'),
+        score = request_data.get('score'),
+        g2a_price = request_data.get('g2aPrice'),
+        g2a_url = request_data.get('g2aUrl'),
+        steam_price = request_data.get('steamPrice'),
+        game_tags = tags
+    )
+    
+    return jsonify(result.serialize()), 200
+
+
+@api.route("/tags", methods=['GET'])
+def get_all_tags():
+    data = db.session.scalars(db.select(Tags)).all()
+    results = list(map(lambda item: item.serialize(), data))
+    response_body = {
+        "results": results
+    }
     return jsonify(response_body), 200
