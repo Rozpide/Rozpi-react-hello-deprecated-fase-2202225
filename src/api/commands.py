4609c82eda07ca717c808958055ae39bb391fbf3
dcrazyx59 <adriaaan.matrix@hotmail.com>
2025-02-13 18:41:13 +0000

    Solución de mergeo guardado

--- src/api/commands.py
+++ src/api/commands.py
@@ -1,7 +1,6 @@
 
 import click
-from api.models import db, User, Games
-import json
+from api.models import db, User
 
 """
 In this file, you can add as many commands as you want using the @app.cli.command decorator
@@ -32,31 +31,4 @@ def setup_commands(app):
 
     @app.cli.command("insert-test-data")
     def insert_test_data():
-        print("Creating test games")
-
-        for x in range(1, 2):
-            # user = Games()
-            # user.email = "test_user" + str(x) + "@test.com"
-            # user.password = "123456"
-            # user.is_active = True
-            game = Games()
-            game.id: Mapped[int] = mapped_column(primary_key=True)
-            name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
-            app_id: Mapped[int] = mapped_column(Integer, nullable=True)
-            release: Mapped[str] = mapped_column(String(100), nullable=True)
-            image_id: Mapped[str] = mapped_column(String(255), nullable=True)
-            score: Mapped[float] = mapped_column(Float, nullable=True)
-            g2a_price: Mapped[float] = mapped_column(Float, nullable=True)
-            g2a_url: Mapped[str] = mapped_column(String(250), nullable=True)
-            steam_price: Mapped[float] = mapped_column(Float, nullable=True)
-            # tags: Mapped[List[String]] = mapped_column(nullable=True)
-            # relación con favoritos
-            favourited: Mapped[List["Favourites"]] = relationship(back_populates="favourite_game")
-            # relación con tags
-            game_tags: Mapped[List["Tags"]] = relationship(secondary=tags_games_association_table, back_populates="games")
-            db.session.add(user)
-            db.session.commit()
-            print("User: ", user.email, " created.")
-
-        print("All test users created")
         pass
